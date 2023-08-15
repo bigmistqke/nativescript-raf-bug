2e912fadecc36098988d5618d14da3e16bbd7544
bigmistqke <vandijckv@gmail.com>
2023-08-15 19:18:02 +0200

    simple raf bug-example

--- app/index.ts
+++ app/index.ts
@@ -2,13 +2,10 @@ import { render } from '@nativescript-community/solid-js'
 import { Application } from '@nativescript/core'
 import { document } from 'dominative'
 import { App } from './app'
-require('@nativescript/canvas-polyfill')
 
 document.body.actionBarHidden = true
 document.body.appendChild(document.createElement('ContentView'))
 
 render(App, document.body.firstElementChild)
 
-const create = () => document
-
-Application.run({ create })
+Application.run({ create: () => document })
